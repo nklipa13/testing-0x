a85c66eb01f5161ba6e68df67543c69572df48db
Nikola Klipa <klipa.nikola@gmail.com>
2020-01-23 15:26:17 +0100

    Add tests

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,45 @@
 const Web3 = require('web3');
 const Dec = require('decimal.js');
+const axios = require('axios');
+const TestContractData = require('../build/contracts/Test.json');
 
 require('dotenv').config();
+
+const NETWORK_ID = '1';
+
+let testContract, account, web3;
+
+const initContracts = async () => {
+    web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_ENDPOINT));
+
+    account = web3.eth.accounts.privateKeyToAccount('0x'+process.env.PRIV_KEY);
+    web3.eth.accounts.wallet.add(account);
+
+    testContract = new web3.eth.Contract(TestContractData.abi, TestContractData.networks[NETWORK_ID].address);
+};
+
+const getPrice = async () => {
+	let res = await axios(`https://api.0x.org/swap/v0/quote?buyToken=DAI&sellToken=WETH&buyAmount=100000000000000000`);
+
+	return { value: res.data.value, addr: res.data.to, data: res.data.data }
+}
+
+const takeOrder = async (addr, amount, data) => {
+    try {
+        const tx = await testContract.methods.takeOrder(addr, data).send({
+            from: account.address, gas: 3000000, value: amount, gasPrice: 6000000000
+        });
+
+        console.log(tx);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+(async () => {
+    await initContracts();
+
+    let res = await getPrice();
+   
+    await takeOrder(res.addr, res.value, res.data);
+})();
